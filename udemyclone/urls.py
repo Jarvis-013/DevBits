8d02ac88ba2a68e836d7533b38fe5bd93b9d4d70
Jarvis-013 <124374386+Jarvis-013@users.noreply.github.com>
2024-04-12 18:54:15 +0530

    Add files via upload

---
+++ udemyclone/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from udemyclone import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls')),
+    path('', include('accounts.urls')),
+    path('', include('udemy.urls')),
+    path('', include('courses.urls')),
+    path('', include('forum.urls')),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
